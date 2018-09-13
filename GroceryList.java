59520b307ed67971b053dc89f9f831ff6008b297
Deepak Sharma <sharma.deepak8616@gmail.com>
2018-09-13 18:42:39 -0400

    Learning push

---
+++ GroceryList.java
@@ -0,0 +1,13 @@
+package Class16;
+
+public class GroceryList {
+
+    public static void main(String[] args) {
+
+        GroceryList_Internal app = new GroceryList_Internal();
+        app.welcomeApp();
+        app.menu();
+    }
+
+
+}
